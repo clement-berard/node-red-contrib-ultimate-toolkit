83ba35959822cadb8e03cad1c12144dc4d5a641c
Clément BERARD <contact@clementberard.com>
2024-12-22 15:14:34 +0100

    up

--- src/common/fns/math-utilities.ts
+++ src/common/fns/math-utilities.ts
@@ -1,8 +1,21 @@
-import { mean, median, round, sum } from 'es-toolkit';
+import { mean, median, sum } from 'es-toolkit';
 
 export const mathUtilities = {
+  abs: Math.abs,
+  acos: Math.acos,
+  asin: Math.asin,
+  atan: Math.atan,
+  ceil: Math.ceil,
+  cos: Math.cos,
+  exp: Math.exp,
+  floor: Math.floor,
+  log: Math.log,
   mean: mean,
   median: median,
-  round: round,
+  random: Math.random,
+  round: Math.round,
+  sin: Math.sin,
   sum: sum,
+  tan: Math.tan,
+  trunc: Math.trunc,
 };
