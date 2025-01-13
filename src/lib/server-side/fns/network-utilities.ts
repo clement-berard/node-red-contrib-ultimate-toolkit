5b4a652073b65cd22729fa9c5348a5de131f47ee
Clément BERARD <contact@clementberard.com>
2025-01-13 13:12:32 +0100

    fix: fix ipInformation function (#17)

--- src/lib/server-side/fns/network-utilities.ts
+++ src/lib/server-side/fns/network-utilities.ts
@@ -2,8 +2,8 @@ import { isIP } from 'node:net';
 import { networkInterfaces } from 'node:os';
 
 export const networkUtilities = {
-  ipInformation: async (input: string) => {
-    if (!isIP(input)) {
+  ipInformation: async (input: string | undefined) => {
+    if (input !== undefined && !isIP(input)) {
       throw new Error('Invalid IP address');
     }
     return (await fetch(`http://ip-api.com/json/${input || ''}`)).json();
